c3e56abbf237c77191491d4aa56ad7c149c94944
Arthur Amaral <arthuramaral6@gmail.com>
2023-01-03 20:11:02 -0300

    first working calculator

--- calculator/main.js
+++ calculator/main.js
@@ -4,43 +4,58 @@ const keys = document.querySelector('#keys');
 const display = document.querySelector('#display p');
 let pressedKeys = [];
 let numbers = [];
+let operator = [];
 
 //Function to display typed operation
 
-function displayOperation() {
+function calculator() {
   keys.addEventListener('click', (e) => {
     console.log(e.target.textContent);
     pressedKeys += e.target.textContent;
     display.textContent = pressedKeys;
 
-    if (e.target.textContent == "+" || e.target.textContent == "-" || e.target.textContent == "x" || e.target.textContent == "/"){
-      numbers.push(pressedKeys.slice(0, pressedKeys.length - 1))
-      console.log(pressedKeys.slice(0, pressedKeys.length - 1));
-      console.log(pressedKeys);
+    if (e.target.textContent === "+" || e.target.textContent === "-" || e.target.textContent === "x" || e.target.textContent === "/"){
+      numbers.push(pressedKeys.slice(0, pressedKeys.length - 1));
+      operator.push(e.target.textContent);
+      pressedKeys = [];
+      display.textContent = pressedKeys;
       console.log(numbers);
-
-      //numbers.push()
+      console.log(operator);
     }
+
+    if (e.target.textContent === "=" && operator == "+") {
+      numbers.push(pressedKeys.slice(0, pressedKeys.length - 1));
+      const result = Number(numbers[0]) + Number(numbers[1]);
+      display.textContent = result;
+    } else if (e.target.textContent === "=" && operator == "-") {
+      numbers.push(pressedKeys.slice(0, pressedKeys.length - 1));
+      const result = Number(numbers[0]) - Number(numbers[1]);
+      display.textContent = result;
+    } else if (e.target.textContent === "=" && operator == "x") {
+      numbers.push(pressedKeys.slice(0, pressedKeys.length - 1));
+      const result = Number(numbers[0]) * Number(numbers[1]);
+      display.textContent = result;
+    } else if (e.target.textContent === "=" && operator == "/") {
+      numbers.push(pressedKeys.slice(0, pressedKeys.length - 1));
+      const result = Number(numbers[0]) / Number(numbers[1]);
+      display.textContent = result;
+    } 
   })  
 }
 
 //Function clear the display
 
-function clearDisplay() {
+function clear() {
   keys.addEventListener('click', (e) => {
     if (e.target.textContent == "AC"){
       pressedKeys = [];
       display.textContent = pressedKeys;
+      numbers = [];
+      operator = [];
     }
   })
 }
 
-//Function to covert string to operation
-
-
-
-
-
-displayOperation();
-clearDisplay();
+calculator();
+clear();
 
