b92af45757b3dc3f8c71b4d82661b253129488c6
Arthur Amaral <arthuramaral6@gmail.com>
2023-01-08 16:18:04 -0300

    continous snake movement

--- snake-game/mine_main.js
+++ snake-game/mine_main.js
@@ -6,10 +6,16 @@ const ctx = canvas.getContext('2d')
 
 const scoreCounter = document.querySelector('header p')
 
+const squareSize = 10
+
 // Function to generate random number
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min
-  return num + (10 - (num % 10))
+  if (num % squareSize === 0) {
+    return num
+  } else {
+    return num + (squareSize - (num % squareSize))
+  }
 }
 
 // Classes to create the squares
@@ -30,7 +36,7 @@ class foodSquare extends Square {
 
   constructor(x, y) {
     super(x, y)
-    this.size = 10
+    this.size = squareSize
     this.color = 'red'
     this.exists = true
   }
@@ -50,24 +56,28 @@ class snakeSquare extends Square {
 
   constructor(x, y) {
     super(x, y)
-    this.size = 10
+    this.size = squareSize
     this.color = 'green'
-    this.velX = 10
-    this.velY = 10
+    this.velX = 0
+    this.velY = 0
 
     window.addEventListener('keydown', e => {
       switch (e.key) {
         case 'ArrowUp':
-          this.y -= this.velY
+          this.velY = -squareSize
+          this.velX = 0
           break
         case 'ArrowDown':
-          this.y += this.velY
+          this.velY = squareSize
+          this.velX = 0
           break
         case 'ArrowLeft':
-          this.x -= this.velX
+          this.velY = 0
+          this.velX = -squareSize
           break
         case 'ArrowRight':
-          this.x += this.velX
+          this.velY = 0
+          this.velX = squareSize
           break
       }
     })
@@ -80,29 +90,55 @@ class snakeSquare extends Square {
   }
 
   checkBounds() {
-    if (this.x < 0 || this.y < 0 || this.x == width || this.y == height) {
+    if (this.x < 0 || this.y < 0 || this.x >= width || this.y >= height) {
       console.log('bateu')
     }
   }
+
+  updatePosition() {
+    if (this.x < 0) {
+      this.x = 0
+      this.velX = 0
+      this.velY = 0
+    } else if (this.y < 0) {
+      this.y = 0
+      this.velX = 0
+      this.velY = 0
+    } else if (this.x >= width) {
+      this.x = width - this.size
+      this.velX = 0
+      this.velY = 0
+    } else if (this.y >= height) {
+      this.y = height - this.size
+      this.velX = 0
+      this.velY = 0
+    }
+
+    this.x += this.velX
+    this.y += this.velY
+  }
 }
 
-const food = new foodSquare(random(0, width - 10), random(0, height - 10))
+const food = new foodSquare(
+  random(0, width - squareSize),
+  random(0, height - squareSize)
+)
 const snake = new snakeSquare(250, 250)
-
 
 function loop() {
   // Painting the canvas white
   ctx.fillStyle = 'white'
   ctx.fillRect(0, 0, width, height)
-  
+
   // Drawing a black square
   ctx.strokeStyle = 'black'
   ctx.lineWidth = 5
   ctx.strokeRect(0, 0, width, height)
-  
+
   food.draw()
   snake.draw()
   snake.checkBounds()
+  snake.updatePosition()
   requestAnimationFrame(loop)
 }
 
