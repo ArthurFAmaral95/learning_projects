9a64c8a6f58e2f1b3693aac09fd2b0d0b729c56a
Arthur Amaral <arthuramaral6@gmail.com>
2023-01-08 15:19:04 -0300

    moving snake and checking of it hits edges

--- snake-game/mine_main.js
+++ snake-game/mine_main.js
@@ -6,15 +6,6 @@ const ctx = canvas.getContext('2d')
 
 const scoreCounter = document.querySelector('header p')
 
-// Painting the canvas white
-ctx.fillStyle = 'white'
-ctx.fillRect(0, 0, width, height)
-
-// Drawing a black square
-ctx.strokeStyle = 'black'
-ctx.lineWidth = 5
-ctx.strokeRect(0, 0, width, height)
-
 // Function to generate random number
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min
@@ -57,23 +48,62 @@ class snakeSquare extends Square {
   velX
   velY
 
-  constructor(x, y, velX, velY) {
+  constructor(x, y) {
     super(x, y)
     this.size = 10
     this.color = 'green'
-    this.velX = velX
-    this.velY = velY
+    this.velX = 10
+    this.velY = 10
+
+    window.addEventListener('keydown', e => {
+      switch (e.key) {
+        case 'ArrowUp':
+          this.y -= this.velY
+          break
+        case 'ArrowDown':
+          this.y += this.velY
+          break
+        case 'ArrowLeft':
+          this.x -= this.velX
+          break
+        case 'ArrowRight':
+          this.x += this.velX
+          break
+      }
+    })
   }
 
   draw() {
     ctx.beginPath()
     ctx.fillStyle = this.color
     ctx.fillRect(this.x, this.y, this.size, this.size)
-  }  
+  }
+
+  checkBounds() {
+    if (this.x < 0 || this.y < 0 || this.x == width || this.y == height) {
+      console.log('bateu')
+    }
+  }
 }
 
 const food = new foodSquare(random(0, width - 10), random(0, height - 10))
-const snake = new snakeSquare(250, 250, 0, 0)
+const snake = new snakeSquare(250, 250)
 
-food.draw()
-snake.draw()
+
+function loop() {
+  // Painting the canvas white
+  ctx.fillStyle = 'white'
+  ctx.fillRect(0, 0, width, height)
+  
+  // Drawing a black square
+  ctx.strokeStyle = 'black'
+  ctx.lineWidth = 5
+  ctx.strokeRect(0, 0, width, height)
+  
+  food.draw()
+  snake.draw()
+  snake.checkBounds()
+  requestAnimationFrame(loop)
+}
+
+loop()
