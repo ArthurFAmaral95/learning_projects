2493d206d30cca20313df0f9044ff5ecb5f668a7
Arthur Amaral <arthuramaral6@gmail.com>
2023-01-08 13:07:57 -0300

    drawing food square

--- snake-game/mine_main.js
+++ snake-game/mine_main.js
@@ -18,5 +18,39 @@ ctx.strokeRect(0, 0, width, height)
 // Function to generate random number
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min
-  return num
+  return num + (10 - (num % 10))
 }
+
+// Classes to create the squares
+class Square {
+  x
+  y
+
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+
+class foodSquare extends Square {
+  size
+  color
+  exists
+
+  constructor(x, y) {
+    super(x, y)
+    this.size = 10
+    this.color = 'red'
+    this.exists = true
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.fillStyle = this.color
+    ctx.fillRect(this.x, this.y, this.size, this.size)
+  }
+}
+
+const food = new foodSquare(random(0, width - 10), random(0, height - 10))
+
+food.draw()
