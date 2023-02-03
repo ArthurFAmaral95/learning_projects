80df6ada12efa9fe0a1f78c3afb2be59b817feb4
Arthur Amaral <arthuramaral6@gmail.com>
2023-02-02 21:18:31 -0300

    key sequecing detection

---
+++ JavaScript30/key-detection/main.js
@@ -0,0 +1,12 @@
+let pressed = []
+const secretCode = 'arthur'
+
+window.addEventListener('keyup', e => {
+  pressed.push(e.key)
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length)
+  console.log(pressed)
+
+  if (pressed.join('').includes(secretCode)) {
+    cornify_add()
+  }
+})
